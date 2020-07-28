4f437e10c64978ceb8e91aa77656b34a3ff00eae
Nick Williams <ntwilliams.personal@gmail.com>
2020-07-28 17:32:49 -0400

    basic atc get function

--- R/api.R
+++ R/api.R
@@ -21,7 +21,70 @@ get_rx <- function(rx_cui) {
   # get response
   res <- httr::GET(url)
   # check response status and return
-  cnt <- response_content(res)
+  cnt <- parse_rx(res)
   class(cnt) <- "rxnorm"
   cnt
 }
+
+#' Get Drug Brand Names From the NLM RxNorm API
+#'
+#' @param rx_cui Either a string or numeric RxNorm RxCUI to search for.
+#'
+#' @return A list of class \code{rxnorm} containing the following components:
+#'
+#' \item{name}{The brand name(s); \code{NULL} if not successful or not applicable.}
+#' \item{id}{The RxNorm RxCUI(s) for the brand name; \code{NULL} if not successful or not applicable.}
+#' \item{url}{The url used for accessing the REST API.}
+#'
+#' @export
+#'
+#' @examples
+#' get_bn(1011485)
+#' get_bn(7052)
+get_bn <- function(rx_cui) {
+  # checks
+  check_internet()
+  # create url
+  url <- paste0(base_url, "rxcui/", rx_cui, "/related?tty=BN")
+  # get response
+  res <- httr::GET(url)
+  # check response status and return
+  cnt <- parse_bn(res)
+  class(cnt) <- "rxnorm"
+  cnt
+}
+
+get_atc <- function(rx_cui, query_atc = c("none", "main", "thera", "pharma", "chem")) {
+  # checks
+  check_internet()
+  # create url
+  url <- paste0(atc_url, rx_cui, "&relaSource=ATC")
+  # get response
+  res <- httr::GET(url)
+  # check response status and return
+  cnt <- parse_atc(res)
+  class(cnt) <- "rxnorm"
+  cnt
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,6 @@
 
 base_url <- "https://rxnav.nlm.nih.gov/REST/"
+atc_url <- "https://rxnav.nlm.nih.gov/REST/rxclass/class/byRxcui.json?rxcui="
 
 check_internet <- function() {
   stopifnot("No internet connection" = curl::has_internet())
@@ -9,7 +10,7 @@ check_status <- function(x) {
   httr::status_code(x) == "200"
 }
 
-response_content <- function(x) {
+parse_rx <- function(x) {
   if (check_status(x)) {
     res <- httr::content(x)
   } else {
@@ -18,5 +19,29 @@ response_content <- function(x) {
 
   list(name = res$idGroup$name,
        id   = res$idGroup$rxnormId[[1]],
+       url  = x$url)
+}
+
+parse_bn <- function(x) {
+  if (check_status(x)) {
+    res <- httr::content(x)
+  } else {
+    res <- NULL
+  }
+
+  list(name = unlist(lapply(res$relatedGroup$conceptGroup[[1]]$conceptProperties, function(x) x$name)),
+       id   = unlist(lapply(res$relatedGroup$conceptGroup[[1]]$conceptProperties, function(x) x$rxcui)),
+       url  = x$url)
+}
+
+parse_atc <- function(x) {
+  if (check_status(x)) {
+    res <- httr::content(x)
+  } else {
+    res <- NULL
+  }
+
+  list(name = res$rxclassDrugInfoList$rxclassDrugInfo[[1]]$rxclassMinConceptItem$classId,
+       id   = NULL,
        url  = x$url)
 }
