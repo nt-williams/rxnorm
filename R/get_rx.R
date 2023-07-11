a2759c82a10c288535079b7294fc4aa085009dd3
nt-williams <ntwilliams.personal@gmail.com>
2023-07-11 09:01:47 -0600

    More robust search for atc from RxCUI and Rx name.

--- R/get_rx.R
+++ R/get_rx.R
@@ -11,11 +11,11 @@
 #' get_rx(1011485)
 get_rx <- function(rx_cui, local_host = FALSE) {
   check_internet()
-  url <- create_url(local_host, paste0("REST/rxcui/", rx_cui))
+  url <- create_url(local_host, paste0("REST/rxcui/", rx_cui, "/historystatus"))
   check_null(parse_rx(httr::GET(url)))
 }
 
 parse_rx <- function(x) {
   if (!check_status(x)) return(NA_character_)
-  httr::content(x, "parse")$idGroup$name
+  httr::content(x, "parse")$rxcuiStatusHistory$attributes$name
 }
